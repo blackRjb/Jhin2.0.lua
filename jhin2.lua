fbc47d64dac518952ed5e6dcba5e035f7920cbd7
blackRjb <vincent74561@gmail.com>
2016-02-04 02:08:11 +0100

    Update jhin2.lua

--- jhin2.lua
+++ jhin2.lua
@@ -17,8 +17,8 @@ JhinMenu.Harass:Boolean("HE", "Use E", false)
 JhinMenu.Harass:Key("Harass1", "Harass", string.byte("C"))
 
 JhinMenu:Menu("Ksecure", "Ksecure")
-JhinMenu.Ksecure:Boolean("KsQ", "Use Q", true)
-JhinMenu.Ksecure:Boolean("KsW", "Use W", true)
+JhinMenu.Ksecure:Boolean("KsQ", "Use Q", false)
+JhinMenu.Ksecure:Boolean("KsW", "Use W", false)
 JhinMenu.Ksecure:Boolean("KsE", "Use E", false)
 
 JhinMenu:Menu("Drawings", "Drawings")
@@ -43,20 +43,6 @@ if JhinMenu.Drawings.W:Value() then
 end
 
 end)
-
-function CastE(unit)
-  local EPred = GetPredictionForPlayer(StartVector3D, target, GetMoveSpeed(target), 1400,999999,750,100,true,true)
-   if EPred.Hitchance == 1 then
-    CastSkillShot(_E,EPred.PredPos)
-   end
-end
-
-function CastW(unit)
-  local WPred = GetPredictionForPlayer(startVector3D, target, GetMoveSpeed(target), 1400, 250, 3000, 220, false, true)
-     if WPred.HitChance == 1 then                
-        CastSkillShot(_W,WPred.PredPos)
-     end
-end
 
  -----COMBO
 OnTick(function(myHero)
@@ -107,15 +93,15 @@ end
 
  
  -----KSECURE
- if KsQ and CanUseSpell(myHero,_Q) == READY and GetHp(target) < getdmg("Q", target) and ValidTarget(target, 550) then 
+ if KsQ and CanUseSpell(myHero,_Q) == READY and GetHp(target) == < getdmg("Q", target) and ValidTarget(target, 550) then 
  	CastTargetSpell(target,_Q)
  end
  
- if KsW and CanUseSpell(myHero,_Q) == READY and GetHP(target) < getdmg("W", target) and ValidTarget(target, 3000) then
+ if KsW and CanUseSpell(myHero,_Q) == READY and GetHP(target) == < getdmg("W", target) and ValidTarget(target, 3000) then
  	CastW(target)
  end
  
- if KsE and CanUseSpell(myHero,_E) == READY and GetHP(target) < getdmg("E", target) and ValidTarget(target, 750) then 
+ if KsE and CanUseSpell(myHero,_E) == READY and GetHP(target) == < getdmg("E", target) and ValidTarget(target, 750) then 
  	CastE(target)
  end
  
